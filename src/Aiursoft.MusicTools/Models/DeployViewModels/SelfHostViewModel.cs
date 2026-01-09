105d472a85b6fe77550dcae5e9b9c21cca61dd99
Anduin Xue <anduin@aiursoft.com>
2026-01-09 12:33:09 +0000

    New selfhost page.

---
+++ src/Aiursoft.MusicTools/Models/DeployViewModels/SelfHostViewModel.cs
@@ -0,0 +1,11 @@
+using Aiursoft.UiStack.Layout;
+
+namespace Aiursoft.MusicTools.Models.DeployViewModels;
+
+public class SelfHostViewModel : UiStackLayoutViewModel
+{
+    public SelfHostViewModel()
+    {
+        PageTitle = "Self Host";
+    }
+}
