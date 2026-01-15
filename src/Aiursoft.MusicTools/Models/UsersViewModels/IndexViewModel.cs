0bec2d58ee5e047ebd709b26327f20f873bd5f90
Anduin Xue <anduin@aiursoft.com>
2026-01-15 09:49:24 +0000

    Pre upgrade.

--- src/Aiursoft.MusicTools/Models/UsersViewModels/IndexViewModel.cs
+++ src/Aiursoft.MusicTools/Models/UsersViewModels/IndexViewModel.cs
@@ -1,13 +1,6 @@
-using Aiursoft.MusicTools.Entities;
 using Aiursoft.UiStack.Layout;
 
 namespace Aiursoft.MusicTools.Models.UsersViewModels;
-
-public class UserWithRolesViewModel
-{
-    public required User User { get; set; }
-    public required IList<string> Roles { get; set; }
-}
 
 public class IndexViewModel : UiStackLayoutViewModel
 {
