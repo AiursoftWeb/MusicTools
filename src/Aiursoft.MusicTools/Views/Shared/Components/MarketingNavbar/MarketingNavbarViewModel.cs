1eb9d1d2e6705afb5ef0a26dc4389e2c16b3b613
Anduin Xue <anduin@aiursoft.com>
2026-02-11 11:56:50 +0000

    Bump template.

--- src/Aiursoft.MusicTools/Views/Shared/Components/MarketingNavbar/MarketingNavbarViewModel.cs
+++ src/Aiursoft.MusicTools/Views/Shared/Components/MarketingNavbar/MarketingNavbarViewModel.cs
@@ -2,5 +2,6 @@ namespace Aiursoft.MusicTools.Views.Shared.Components.MarketingNavbar;
 
 public class MarketingNavbarViewModel
 {
-
+    public string ProjectName { get; set; } = "Aiursoft MusicTools";
+    public string LogoUrl { get; set; } = "/logo.svg";
 }
