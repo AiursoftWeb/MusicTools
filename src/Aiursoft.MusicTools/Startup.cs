078ac6f3ef54b6f938720a1119a18fcd318232d5
Anduin Xue <anduin@aiursoft.com>
2026-04-06 10:06:13 +0000

    Refactor background job system: replace BackgroundJobQueue with ServiceTaskQueue, add DummyJob and OrphanAvatarCleanupJob, and update JobsController and related models for new task management

--- src/Aiursoft.MusicTools/Startup.cs
+++ src/Aiursoft.MusicTools/Startup.cs
@@ -13,6 +13,9 @@ using Microsoft.AspNetCore.Mvc.Razor;
 using Aiursoft.ClickhouseLoggerProvider;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Serialization;
+using Aiursoft.Canon.TaskQueue;
+using Aiursoft.Canon.BackgroundJobs;
+using Aiursoft.Canon.ScheduledTasks;
 
 namespace Aiursoft.MusicTools;
 
@@ -50,8 +53,11 @@ public class Startup : IWebStartup
         services.AddSingleton<NavigationState<Startup>>();
 
         // Background job queue
-        services.AddSingleton<Services.BackgroundJobs.BackgroundJobQueue>();
-        services.AddHostedService<Services.BackgroundJobs.QueueWorkerService>();
+        services.AddTaskQueueEngine();
+        services.AddScheduledTaskEngine();
+        services.RegisterBackgroundJob<Services.BackgroundJobs.DummyJob>();
+        var orphanAvatarCleanupJob = services.RegisterBackgroundJob<Services.BackgroundJobs.OrphanAvatarCleanupJob>();
+        services.RegisterScheduledTask(registration: orphanAvatarCleanupJob, period: TimeSpan.FromHours(6), startDelay: TimeSpan.FromMinutes(5));
 
         // Controllers and localization
         services.AddControllersWithViews()
